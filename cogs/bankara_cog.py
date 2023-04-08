23b66089c3b49c38d8064547001e9b1d30349fe5
Chroma7p <nahco3.hcl@gmail.com>
2023-04-08 16:00:39 +0900

    スラッシュコマンド対応

--- cogs/bankara_cog.py
+++ cogs/bankara_cog.py
@@ -1,25 +1,31 @@
 from discord.ext import commands,tasks
+from discord import app_commands
+import discord
 from get_bankara_info import maketext
 
 class BankaraCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    # Cogが読み込まれた時に発動
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('BankaraCog on ready!')
+
     # コマンドの記述
-    @commands.command()
-    async def open(self, ctx):
+    @app_commands.command(name="open",description="現在のバンカラマッチ(オープン)の情報を表示します。")
+    async def open(self, interaction:discord.Interaction):
         txt=maketext(is_open=True)
-        await ctx.send(txt)
+        await interaction.response.send_message(txt)
     
-    @commands.command()
-    async def challenge(self, ctx):
+    @app_commands.command(name="challenge",description="現在のバンカラマッチ(チャレンジ)の情報を表示します。")
+    async def challenge(self, interaction:discord.Interaction):
         txt=maketext(is_open=False)
-        await ctx.send(txt)
+        await interaction.response.send_message(txt)
 
 
 
 
 # Cogとして使うのに必要なsetup関数
 def setup(bot):
-    print("BankaraCog OK")
     return bot.add_cog(BankaraCog(bot))
--- discordbot.py
+++ discordbot.py
@@ -5,6 +5,9 @@ from cogs.regular_cog import RegularCog
 from cogs.bankara_cog import BankaraCog
 import os
 import discord
+from dotenv import load_dotenv
+import asyncio
+load_dotenv(".env")
 
 cogs = [SalmonCog,RegularCog,BankaraCog]
 
@@ -12,19 +15,31 @@ APITOKEN= os.environ["SPLATOON_BOT_TOKEN"]
 
 
 # botのオブジェクトを作成(コマンドのトリガーを!に)
-bot = commands.Bot(command_prefix="!",intents=discord.Intents.all())
+bot = commands.Bot(command_prefix="/",intents=discord.Intents.all(), application_id=os.environ["SPLATOON_BOT_ID"])
 
 
 # イベントを検知
 @bot.event
 # botの起動が完了したとき
 async def on_ready():
-    for cog in cogs:
-        await bot.add_cog(cog(bot))
+    await bot.tree.sync()
     print("Hello!")  # コマンドラインにHello!と出力
 
 
+async def main():
+    # コグのフォルダ
+    cogfolder = "cogs."
+    # そして使用するコグの列挙(拡張子無しのファイル名)
+    cogs = ["bankara_cog", "regular_cog", "salmon_cog"]
 
+    for c in cogs:
+        await bot.load_extension(cogfolder + c)
+
+    # start the client
+    async with bot:
+        await bot.start(APITOKEN)
+
+asyncio.run(main())
 
 
 

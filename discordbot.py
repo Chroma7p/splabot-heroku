386874f5e3009c97a8731a70813299aa0ee72908
chroma7p <nahco3.hcl@gmail.com>
2023-07-01 17:03:44 +0900

    Xマッチ情報取得用コマンド追加

--- discordbot.py
+++ discordbot.py
@@ -1,5 +1,5 @@
 # discord.pyの大事な部分をimport
-from discord.ext import commands,tasks
+from discord.ext import commands
 from cogs.salmon_cog import SalmonCog
 from cogs.regular_cog import RegularCog
 from cogs.bankara_cog import BankaraCog
@@ -31,7 +31,7 @@ async def main():
     # コグのフォルダ
     cogfolder = "cogs."
     # そして使用するコグの列挙(拡張子無しのファイル名)
-    cogs = ["bankara_cog", "regular_cog", "salmon_cog" ,"weapon_cog"]
+    cogs = ["bankara_cog", "regular_cog", "salmon_cog" ,"weapon_cog","x_cog"]
 
     for c in cogs:
         await bot.load_extension(cogfolder + c)

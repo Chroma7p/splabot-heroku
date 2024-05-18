21869a16735f7b6156f9f7ef740f10d77a6893cb
Chroma7p <nahco3.hcl@gmail.com>
2024-05-18 16:05:03 +0900

    フェス期間のバンカラ表示を修正

--- src/cogs/util/get_bankara_info.py
+++ src/cogs/util/get_bankara_info.py
@@ -11,7 +11,24 @@ def maketext(is_open=True):
         text = "## **バンカラチャレンジ**\n"
     if "fail" in schedule:
         return "情報の取得に失敗しました"
+    print(schedule)
+    if not schedule:
+        try:
+            fest_schedule = get_spla_info("regular", "schedule")
+            fs = fest_schedule[0]
+            if not fs["is_fest"]:
+                raise "フェス期間じゃないけどバンカラが取得できてなさそう"
+            start_time = datetime.strptime(
+                fs["start_time"], "%Y-%m-%dT%H:%M:%S%z")
+            end_time = datetime.strptime(fs["end_time"], "%Y-%m-%dT%H:%M:%S%z")
+            text += f"**フェス期間だからないよ！**\n"
+            text += "**フェス期間**\n"
+            text += f"{start_time.strftime('%Y-%m-%d %H:%M')}~{end_time.strftime('%Y-%m-%d %H:%M')}\n"
 
+        except Exception as e:
+            return f"想定外のエラーみたいだね\n以下のメッセージを開発者に教えてね\n```{e.with_traceback()}```"
+
+        return text+"**もしフェス期間じゃない場合は開発者に教えてね**"
     for i, day in enumerate(schedule[:3]):
         if i == 0:
             text += "### **現在**\n"
@@ -22,8 +39,10 @@ def maketext(is_open=True):
         if not day["is_fest"]:
             rule = day["rule"]
             text += f"__***{rule['name']}***__\n"
-        start: datetime = datetime.strptime(day["start_time"], "%Y-%m-%dT%H:%M:%S%z")
-        end: datetime = datetime.strptime(day["end_time"], "%Y-%m-%dT%H:%M:%S%z")
+        start: datetime = datetime.strptime(
+            day["start_time"], "%Y-%m-%dT%H:%M:%S%z")
+        end: datetime = datetime.strptime(
+            day["end_time"], "%Y-%m-%dT%H:%M:%S%z")
         text += f"{start.strftime('%Y-%m-%d %H:%M')}~{end.strftime('%Y-%m-%d %H:%M')}\n"
         if day["is_fest"]:
             text += "フェス期間だからないよ！\n"
